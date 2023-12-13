18e729efb106ce71f64bf6c9babc534eab1bf637
Ryan Kamleiter <kamleiterryan@gmail.com>
2023-12-13 13:45:35 -0600

    questions 1-3 working as intended

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -54,10 +54,27 @@ for (star of stars) {
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
-console.log('Some stars using while:');
+let index = 0;
+let max = 5;
+while(index < max){
+  console.log(stars[index]);
+  index ++;
+}
+
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
+let i = 0;
+while(i < 6) {
+  console.log(i);
+  i ++;
+}
+
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+i = 10;
+while(i > 4) {
+  console.log(i);
+  i --;
+}
